e55c275fc7750fbaef00f1d8741dd0162b54544c
GilangKun <49320620+GilangKun@users.noreply.github.com>
2019-04-08 13:59:34 +0700

    Add files via upload

---
+++ Hek.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/bash
+#tools sederhana
+#prank hack satelit
+#by LxG_9 Gans
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|======================================|>"
+echo  $red    "|       TOOLS HACK SATELIT          |"
+echo  $red    "|                                   |"
+echo  $red    "| Author        : LxG_9             |"
+echo  $red    "| Contact       : 081226988596      |"
+echo  $red    "| My Team       : KiLLeR Cyber Team |"
+echo  $red    "| Thanks To     : Mr.Devil          |"
+echo  $blue "<|======================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Geser Satelit Palapa       $blue ||"
+echo $blue "|| $green 2. Geser Satelit Telkom       $blue ||"
+echo $blue "|| $green 3. Geser Satelit Asiasat      $blue ||"
+echo $blue "|| $green 4. Geser Satelit Nasa         $blue ||"
+echo $blue "|| $green 0. Exit The Program           $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@GilangKun=•>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@GilangKun=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@GilangKun=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasi  Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@GilangKun=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@GilangKun=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@GilangKun=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@GilangKun=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@GilangKun=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@GilangKun=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "Bay Bay"
+echo  $blue "Jangan Lupa Coli Hari Ini!"
+echo  $green "KiLLeR Cyber Team!"
+exit
+fi
